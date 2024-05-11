82be0d4343bf623b0f3519c5f8b5e9b621d81da1
ryichk <ryichk@gmail.com>
2024-05-11 20:19:47 +0900

    fix: Q12で、桁数の多い浮動小数点数の文字列変換を確実に行うためにsprintf('%10.10f', x)を使用するように修正

--- Ruby/q12_square_root_number.rb
+++ Ruby/q12_square_root_number.rb
@@ -4,7 +4,8 @@
 # 整数部分を含む場合
 n = 2
 while
-  sqrts = Math.sqrt(n).to_s.chars
+  # 桁数の多い浮動小数点数を確実に文字列変換するためにsprintf('%10.10f', x)を使用している
+  sqrts = sprintf('%10.10f', Math.sqrt(n)).chars
   numbers = sqrts.reject { |i| i == '.' }[0..9].map(&:to_i).sort
   if numbers == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     p n
@@ -16,7 +17,7 @@ end
 # 小数部分のみの場合
 n = 2
 while
-  sqrts = Math.sqrt(n).to_s.chars
+  sqrts = sprintf('%10.10f', Math.sqrt(n)).chars
   numbers = sqrts[sqrts.index('.')+1..sqrts.index('.')+10].map(&:to_i).sort
   if numbers == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     p n
